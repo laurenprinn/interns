285c431839623e6730486a91c79660ed3e03d0f6
dakamin1 <dakamin.mtu.edu>
2019-06-16 20:09:02 -0400

    added page to lay the ground work for added development

--- internGame/app/src/main/java/com/example/interngame/MainActivity.java
+++ internGame/app/src/main/java/com/example/interngame/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.interngame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button start = findViewById(R.id.startButton);
+
+        start.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                setContentView(R.layout.game_started);
+            }
+        });
+
     }
 }
